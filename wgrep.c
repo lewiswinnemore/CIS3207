2dc8cbd4fbfb65acbd5319d39f4140adcc667b30
lewiswinnemore <78185203+lewiswinnemore@users.noreply.github.com>
2021-01-28 21:15:42 -0500

    Create wgrep.c

---
+++ wgrep.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 999
+
+int main(int argc, char *argv){
+
+        char buffer[SIZE];
+
+        printf("enter search term");
+        scanf("%s", mah);
+        for(int i = 0; i<SIZE; i++){
+        FILE *fp = fopen("wgrep.c", "r");        //copied from lab pdf
+        if(fp == NULL){
+                printf("cannot open file\n");
+                exit(1);
+
+        }
+
+        //char get = *fgets(buffer, SIZE, fp);
+
+while(fgets(buffer, SIZE, fp) != NULL){
+        int f++;
+
+       
+}
+
+        printf("\n");
+        fclose(fp);
+}
+
+
+
+
+}
